e58d560a911966bddb57359d18c53f7f9dbd6966
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-20 08:46:47 +0900

    リファクタリングと、ローカルの食材データソースに食材追加など

--- Sample_CodingTest1/CodingTestAnswer/ImprovementSuggestions/NaturalLanguageMealRecordCreation/NaturalLanguageMealRecordCreationViewModel.swift
+++ Sample_CodingTest1/CodingTestAnswer/ImprovementSuggestions/NaturalLanguageMealRecordCreation/NaturalLanguageMealRecordCreationViewModel.swift
@@ -45,48 +45,13 @@ final class NaturalLanguageMealRecordCreationViewModel {
             isCreating = true
             
             do {
-                let queryText = """
-
-次のような形式の文章が渡されます。それを下記の（例）の感じで、食材をクエリとして抜き出すのと、文章の中で言及された器のサイズや内容量などの情報を元にして、それぞれの食材のおおまかな食べた量を推測してグラムの数値で示してください。つまり、器が小さければそれぞれの食材のグラム数は少ないはずで、逆に器が大きければたくさん入るので各食材のグラム数も多くなります。それを考慮した上で回答を生成してください。
-
-（例1：「キムチ納豆ご飯を、小さめのお茶碗で食べた。あとはメカブを１パック。」）
-この場合は下記の様な形式で返して。
-[
-    {"name": "キムチ", "grams": 80},
-    {"name": "納豆", "grams": 40},
-    {"name": "白米", "grams": 120},
-    {"name": "メカブ", "grams": 40}
-]
-
-[{"キムチ": 80}, {"納豆": 40}, {"白米": 120}, {"メカブ": 40}]
-
-
-（例2：「スライストマト、バジル、モッツァレラチーズにオリーブをかけたサラダをラーメンどんぶりいっぱいに盛って食べた。」）
-この場合は下記の様な形式で返して。
-
-[
-    {"name": "トマト", "grams": 200},
-    {"name": "バジル", "grams": 60},
-    {"name": "モッツァレラチーズ", "grams": 300},
-    {"name": "オリーブオイル", "grams": 10}
-]
-
-例の説明はここで終わりです。
-
-それでは、下記が今回の対象の文章です。
-↓↓↓
-
-『\(inputText)』
-
-"""
+                let queryText = systemPromptText + "『\(inputText)』"
                 // OpenAI APIのChatCompletionAPIを叩く
                 let resultMessageText = try await repository.sendChat(queryText: queryText)
-                print("resultMessageText: \(resultMessageText)")
                 
                 guard let resultJsonData: Data = resultMessageText.data(using: .utf8) else { return }
                 // JSONから構造体にパースする
                 let parsedIngredients = try JSONDecoder().decode([Ingredient].self, from: resultJsonData)
-                print("parsedIngredients: \(parsedIngredients)")
                 
                 // TODO: VectorStoreなどを使ったベクトル類似度検索が今回間に合わなかったが、それをすれば文字列の一致よりももっと柔軟に食材データベースとの称号ができていい感じにできる気がする
                 
@@ -103,15 +68,13 @@ final class NaturalLanguageMealRecordCreationViewModel {
                         consumedIngredientListItems.append(consumedIngredientListItem)
                         
                     } else {
-                        print("一致なし parsedIngredient.name: \(parsedIngredient.name)")
+                        print("ローカルの食材データソースとの一致なし parsedIngredient.name: \(parsedIngredient.name)")
                     }
                 }
-                print("forEach後のconsumedIngredientListItems: \(consumedIngredientListItems)")
                 totalKcal = consumedIngredientListItems.reduce(into: 0) { _totalKcal, item in
                     // 各食材ごとの1gあたりのPFCをDBデータに保持しているので、それに食べたグラム数を渡して総カロリーを求める
                     _totalKcal += item.ingredient.getTotalKcal(for: item.consumedGrams)
                 }
-                print("totalKcal更新した: \(totalKcal)")
                 
             } catch {
                 print("repository.sendChat error: \(error)")
--- Sample_CodingTest1/CodingTestAnswer/ImprovementSuggestions/NaturalLanguageMealRecordCreation/PerGramIngredientsDataSource.swift
+++ Sample_CodingTest1/CodingTestAnswer/ImprovementSuggestions/NaturalLanguageMealRecordCreation/PerGramIngredientsDataSource.swift
@@ -67,5 +67,137 @@ final class PerGramIngredientsDataSource {
             fatPer1Gram: 0.002,
             carbohydratePer1Gram: 0.039
         ),
+        .init(
+            name: "ごはん",
+            proteinPer1Gram: 0.025,
+            fatPer1Gram: 0.003,
+            carbohydratePer1Gram: 0.28
+        ),
+        .init(
+            name: "味噌汁",
+            proteinPer1Gram: 0.025,
+            fatPer1Gram: 0.011,
+            carbohydratePer1Gram: 0.034
+        ),
+        .init(
+            name: "豆腐",
+            proteinPer1Gram: 0.08,
+            fatPer1Gram: 0.05,
+            carbohydratePer1Gram: 0.02
+        ),
+        .init(
+            name: "納豆",
+            proteinPer1Gram: 0.18,
+            fatPer1Gram: 0.11,
+            carbohydratePer1Gram: 0.14
+        ),
+        .init(
+            name: "鮭",
+            proteinPer1Gram: 0.20,
+            fatPer1Gram: 0.06,
+            carbohydratePer1Gram: 0.0
+        ),
+        .init(
+            name: "うなぎ",
+            proteinPer1Gram: 0.19,
+            fatPer1Gram: 0.18,
+            carbohydratePer1Gram: 0.01
+        ),
+        .init(
+            name: "卵",
+            proteinPer1Gram: 0.13,
+            fatPer1Gram: 0.11,
+            carbohydratePer1Gram: 0.01
+        ),
+        .init(
+            name: "枝豆",
+            proteinPer1Gram: 0.11,
+            fatPer1Gram: 0.06,
+            carbohydratePer1Gram: 0.07
+        ),
+        .init(
+            name: "かつお節",
+            proteinPer1Gram: 0.76,
+            fatPer1Gram: 0.02,
+            carbohydratePer1Gram: 0.0
+        ),
+        .init(
+            name: "こんにゃく",
+            proteinPer1Gram: 0.006,
+            fatPer1Gram: 0.0005,
+            carbohydratePer1Gram: 0.03
+        ),
+        .init(
+            name: "切り干し大根",
+            proteinPer1Gram: 0.04,
+            fatPer1Gram: 0.01,
+            carbohydratePer1Gram: 0.8
+        ),
+        .init(
+            name: "海苔",
+            proteinPer1Gram: 0.5,
+            fatPer1Gram: 0.03,
+            carbohydratePer1Gram: 0.34
+        ),
+        .init(
+            name: "キャベツ",
+            proteinPer1Gram: 0.012,
+            fatPer1Gram: 0.001,
+            carbohydratePer1Gram: 0.05
+        ),
+        .init(
+            name: "きゅうり",
+            proteinPer1Gram: 0.0065,
+            fatPer1Gram: 0.001,
+            carbohydratePer1Gram: 0.037
+        ),
+        .init(
+            name: "トマト",
+            proteinPer1Gram: 0.009,
+            fatPer1Gram: 0.002,
+            carbohydratePer1Gram: 0.04
+        ),
+        .init(
+            name: "リンゴ",
+            proteinPer1Gram: 0.0025,
+            fatPer1Gram: 0.003,
+            carbohydratePer1Gram: 0.12
+        ),
+        .init(
+            name: "バナナ",
+            proteinPer1Gram: 0.011,
+            fatPer1Gram: 0.0032,
+            carbohydratePer1Gram: 0.22
+        ),
+        .init(
+            name: "いちご",
+            proteinPer1Gram: 0.0078,
+            fatPer1Gram: 0.003,
+            carbohydratePer1Gram: 0.08
+        ),
+        .init(
+            name: "柿",
+            proteinPer1Gram: 0.006,
+            fatPer1Gram: 0.003,
+            carbohydratePer1Gram: 0.18
+        ),
+        .init(
+            name: "さつまいも",
+            proteinPer1Gram: 0.015,
+            fatPer1Gram: 0.01,
+            carbohydratePer1Gram: 0.28
+        ),
+        .init(
+            name: "じゃがいも",
+            proteinPer1Gram: 0.02,
+            fatPer1Gram: 0.001,
+            carbohydratePer1Gram: 0.17
+        ),
+        .init(
+            name: "スイートコーン",
+            proteinPer1Gram: 0.03,
+            fatPer1Gram: 0.02,
+            carbohydratePer1Gram: 0.19
+        ),
     ]
 }
