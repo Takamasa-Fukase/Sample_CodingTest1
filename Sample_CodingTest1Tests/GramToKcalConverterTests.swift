dc68613609fc189613d8ee34f1300c56ec9e26a2
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-19 17:56:38 +0900

    テストファイルを追加

---
+++ Sample_CodingTest1Tests/GramToKcalConverterTests.swift
@@ -0,0 +1,15 @@
+//
+//  GramToKcalConverterTests.swift
+//  Sample_CodingTest1Tests
+//
+//  Created by ウルトラ深瀬 on 19/2/25.
+//
+
+import XCTest
+@testable import Sample_CodingTest1
+
+final class GramToKcalConverterTests: XCTestCase {
+    func test_example() {
+        
+    }
+}
