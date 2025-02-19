2fc247ca2eec437aa751cdb92a8c23a17c5c6a0b
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-19 12:31:29 +0900

    タブアイコン追加

--- Sample_CodingTest1/ContentView.swift
+++ Sample_CodingTest1/ContentView.swift
@@ -11,10 +11,10 @@ struct ContentView: View {
     var body: some View {
         TabView() {
             Group {
-                Tab("合計カロリー計算", image: "") {
+                Tab("合計カロリー計算", systemImage: "123.rectangle") {
                     TotalKcalSimulatorView()
                 }
-                Tab("自然言語お手軽食事記録作成", image: "") {
+                Tab("自然言語お手軽食事記録作成", systemImage: "rectangle.and.pencil.and.ellipsis") {
                     NaturalLanguageMealRecordCreationView()
                 }
             }
