5ca59ded2e49b5a33e72ff3d24919d695ee09e99
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-19 14:25:22 +0900

    extensionへの移動と、gを少数第二位を明示的に四捨五入する処理を入れている途中まで。（多分ロジックが間違っているがビルド通るところまで）

--- Sample_CodingTest1/CodingTestAnswer/CodingTestAnswerLogic/GramToKcalConverter.swift
+++ Sample_CodingTest1/CodingTestAnswer/CodingTestAnswerLogic/GramToKcalConverter.swift
@@ -13,18 +13,32 @@ final class GramToKcalConverter {
     private static let kcalPer1GramOfCarbohydrate: Int = 4  // 炭水化物　 1(g) = 4(kcal)
     
     static func getProteinKcal(from grams: Double) -> Int {
-        return convertToRoundedInt(doubleKcal: grams * Double(kcalPer1GramOfProtein))
+        return convert(from: grams, with: kcalPer1GramOfProtein)
     }
     
     static func getFatKcal(from grams: Double) -> Int {
-        return convertToRoundedInt(doubleKcal: grams * Double(kcalPer1GramOfFat))
+        return convert(from: grams, with: kcalPer1GramOfFat)
     }
     
     static func getCarbohydrateKcal(from grams: Double) -> Int {
-        return convertToRoundedInt(doubleKcal: grams * Double(kcalPer1GramOfCarbohydrate))
+        return convert(from: grams, with: kcalPer1GramOfCarbohydrate)
     }
-        
-    private static func convertToRoundedInt(doubleKcal: Double) -> Int {
-        return Int(round(doubleKcal))
+    
+    private static func convert(from grams: Double, with kcalPer1Gram: Int) -> Int {
+        return (grams.intakeGramsDouble * Double(kcalPer1Gram)).roundedInt
+    }
+}
+
+extension Double {
+    // ● 摂取量(g) : 小数第一位 (小数第二位を四捨五入)
+    var intakeGramsDouble: Double {
+        print("intakeGramsDouble 元の値: \(self), 変換後: \((self * 10).rounded())")
+        return (self * 10).rounded()
+    }
+    
+    // ● カロリー(kcal) : 整数 (小数第一位を四捨五入)
+    var roundedInt: Int {
+        print("roundedInt 元の値: \(self), 変換後: \(Int(self.rounded()))")
+        return Int(self.rounded())
     }
 }
