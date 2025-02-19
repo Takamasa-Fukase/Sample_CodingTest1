f458b0aa6e41f17a8d9e14ee75c62d75f3b2fd05
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-19 22:43:28 +0900

    Int型であることの明示を追加

--- Sample_CodingTest1Tests/GramToKcalConverterTests.swift
+++ Sample_CodingTest1Tests/GramToKcalConverterTests.swift
@@ -31,7 +31,7 @@ final class GramToKcalConverterTests: XCTestCase {
         // MARK: Protein
         // 下半分側（例：0.00~0.04 => 0.0）の範囲での切り捨てが行われていることを確認する
         let proteinGramsOfToRoundDown: Double = 1.125
-        let proteinKcalOfRoundedDown = GramToKcalConverter.getProteinKcal(for: proteinGramsOfToRoundDown)
+        let proteinKcalOfRoundedDown: Int = GramToKcalConverter.getProteinKcal(for: proteinGramsOfToRoundDown)
         // 1.125 x 4 = 4.5 だと繰り上がって、結果が5になるはず。
         // 但し、実際には1.125 は 1.1に四捨五入された状態で計算が行われるので、
         // 1.1   x 4 = 4.4 で切り捨てになって、結果が4になることを確認する。
@@ -39,7 +39,7 @@ final class GramToKcalConverterTests: XCTestCase {
         
         // 上半分側（例：0.05~0.10 => 0.1）の範囲での繰り上げが行われていることを確認する
         let proteinGramsOfToRoundUp: Double = 1.35
-        let proteinKcalOfRoundedUp = GramToKcalConverter.getProteinKcal(for: proteinGramsOfToRoundUp)
+        let proteinKcalOfRoundedUp: Int = GramToKcalConverter.getProteinKcal(for: proteinGramsOfToRoundUp)
         // 1.35 x 4 = 5.4 だと切り捨てになって、結果が5になるはず。
         // 但し、実際には1.35 は 1.4に四捨五入された状態で計算が行われるので、
         // 1.4  x 4 = 5.6 で繰り上がって、結果が6になることを確認する。
@@ -49,7 +49,7 @@ final class GramToKcalConverterTests: XCTestCase {
         // MARK: Fat
         // 下半分側（例：0.00~0.04 => 0.0）の範囲での切り捨てが行われていることを確認する
         let fatGramsOfToRoundDown: Double = 1.74
-        let fatKcalOfRoundedDown = GramToKcalConverter.getFatKcal(for: fatGramsOfToRoundDown)
+        let fatKcalOfRoundedDown: Int = GramToKcalConverter.getFatKcal(for: fatGramsOfToRoundDown)
         // 1.74 x 9 = 15.66 だと繰り上がって、結果が16になるはず。
         // 但し、実際には1.74 は 1.7に四捨五入された状態で計算が行われるので、
         // 1.7  x 9 = 15.3  で切り捨てになって、結果が15になることを確認する。
@@ -57,7 +57,7 @@ final class GramToKcalConverterTests: XCTestCase {
         
         // 上半分側（例：0.05~0.10 => 0.1）の範囲での繰り上げが行われていることを確認する
         let fatGramsOfToRoundUp: Double = 1.45
-        let fatKcalOfRoundedUp = GramToKcalConverter.getFatKcal(for: fatGramsOfToRoundUp)
+        let fatKcalOfRoundedUp: Int = GramToKcalConverter.getFatKcal(for: fatGramsOfToRoundUp)
         // 1.45 x 9 = 13.05 だと切り捨てになって、結果が13になるはず。
         // 但し、実際には1.45 は 1.5に四捨五入された状態で計算が行われるので、
         // 1.5  x 9 = 13.5  で繰り上がって、結果が14になることを確認する。
@@ -67,7 +67,7 @@ final class GramToKcalConverterTests: XCTestCase {
         // MARK: Carbohydrate
         // 下半分側（例：0.00~0.04 => 0.0）の範囲での切り捨てが行われていることを確認する
         let carbohydrateGramsOfToRoundDown: Double = 1.125
-        let carbohydrateKcalOfRoundedDown = GramToKcalConverter.getCarbohydrateKcal(for: carbohydrateGramsOfToRoundDown)
+        let carbohydrateKcalOfRoundedDown: Int = GramToKcalConverter.getCarbohydrateKcal(for: carbohydrateGramsOfToRoundDown)
         // 1.125 x 4 = 4.5 だと繰り上がって、結果が5になるはず。
         // 但し、実際には1.125 は 1.1に四捨五入された状態で計算が行われるので、
         // 1.1   x 4 = 4.4 で切り捨てになって、結果が4になることを確認する。
@@ -75,7 +75,7 @@ final class GramToKcalConverterTests: XCTestCase {
         
         // 上半分側（例：0.05~0.10 => 0.1）の範囲での繰り上げが行われていることを確認する
         let carbohydrateGramsOfToRoundUp: Double = 1.35
-        let carbohydrateKcalOfRoundedUp = GramToKcalConverter.getCarbohydrateKcal(for: carbohydrateGramsOfToRoundUp)
+        let carbohydrateKcalOfRoundedUp: Int = GramToKcalConverter.getCarbohydrateKcal(for: carbohydrateGramsOfToRoundUp)
         // 1.35 x 4 = 5.4 だと切り捨てになって、結果が5になるはず。
         // 但し、実際には1.35 は 1.4に四捨五入された状態で計算が行われるので、
         // 1.4  x 4 = 5.6 で繰り上がって、結果が6になることを確認する。
@@ -91,15 +91,15 @@ final class GramToKcalConverterTests: XCTestCase {
         // 脂質　　　 1(g) = 9(kcal)
         // 炭水化物　 1(g) = 4(kcal)
         
-        let proteinKcalFor3Grams = GramToKcalConverter.getProteinKcal(for: 3)
+        let proteinKcalFor3Grams: Int = GramToKcalConverter.getProteinKcal(for: 3)
         // 3 x 4 = 12
         XCTAssertEqual(proteinKcalFor3Grams, 12)
         
-        let fatKcalFor3Grams = GramToKcalConverter.getFatKcal(for: 4)
+        let fatKcalFor3Grams: Int = GramToKcalConverter.getFatKcal(for: 4)
         // 4 x 9 = 36
         XCTAssertEqual(fatKcalFor3Grams, 36)
         
-        let carbohydrateKcalFor3Grams = GramToKcalConverter.getCarbohydrateKcal(for: 5)
+        let carbohydrateKcalFor3Grams: Int = GramToKcalConverter.getCarbohydrateKcal(for: 5)
         // 5 x 4 = 20
         XCTAssertEqual(carbohydrateKcalFor3Grams, 20)
     }
